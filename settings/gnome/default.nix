8ad02e54cc07b9b4f30e2156b25773a0bf33996a
Ewan Croft <git@ewancroft.uk>
2026-02-08 19:46:20 +0000

    dconf-export: 2026-02-08 19:46:20

--- settings/gnome/default.nix
+++ settings/gnome/default.nix
@@ -1,29 +1,6 @@
 { ... }:
 {
   imports = [
-    ./desktop/app-folders/app-folders.nix
-    ./desktop/app-folders/folders/Pardus/Pardus.nix
-    ./desktop/app-folders/folders/System/System.nix
-    ./desktop/app-folders/folders/Utilities/Utilities.nix
-    ./desktop/app-folders/folders/YaST/YaST.nix
-    ./desktop/background/background.nix
-    ./desktop/input-sources/input-sources.nix
-    ./desktop/interface/interface.nix
-    ./desktop/notifications/application/firefox/firefox.nix
-    ./desktop/notifications/application/gnome-about-panel/gnome-about-panel.nix
-    ./desktop/notifications/application/gnome-power-panel/gnome-power-panel.nix
-    ./desktop/notifications/notifications.nix
-    ./desktop/screensaver/screensaver.nix
-    ./desktop/wm/preferences/preferences.nix
-    ./evolution-data-server/evolution-data-server.nix
-    ./housekeeping/housekeeping.nix
-    ./nautilus/preferences/preferences.nix
-    ./nautilus/window-state/window-state.nix
-    ./plugins/housekeeping/housekeeping.nix
-    ./settings/file-chooser/file-chooser.nix
     ./shell/shell.nix
-    ./shell/world-clocks/world-clocks.nix
-    ./terminal/legacy/legacy.nix
-    ./terminal/legacy/profiles-/-b1dcc9dd-5262-4d8d-a863-c897e6d979b9/-b1dcc9dd-5262-4d8d-a863-c897e6d979b9.nix
   ];
 }
