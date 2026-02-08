36e58584357b11b30d70de5c9995a50827f5ce1d
Ewan Croft <git@ewancroft.uk>
2026-02-08 19:42:05 +0000

    dconf-export: 2026-02-08 19:42:05

--- settings/gnome/shell/shell.nix
+++ settings/gnome/shell/shell.nix
@@ -1,10 +1,10 @@
 { ... }:
 {
   dconf.settings."/org/gnome/shell" = {
-    "app-picker-layout" = [{'org.gnome.Contacts.desktop': <{'position': <0>}>, 'org.gnome.Weather.desktop': <{'position': <1>}>, 'org.gnome.clocks.desktop': <{'position': <2>}>, 'org.gnome.Maps.desktop': <{'position': <3>}>, 'org.gnome.SimpleScan.desktop': <{'position': <4>}>, 'org.gnome.Settings.desktop': <{'position': <5>}>, 'org.gnome.Showtime.desktop': <{'position': <6>}>, 'org.gnome.Snapshot.desktop': <{'position': <7>}>, 'Utilities': <{'position': <8>}>, 'System': <{'position': <9>}>, 'org.gnome.Console.desktop': <{'position': <10>}>, 'org.gnome.Yelp.desktop': <{'position': <11>}>, 'org.gnome.Calculator.desktop': <{'position': <12>}>, 'org.gnome.Extensions.desktop': <{'position': <13>}>, 'htop.desktop': <{'position': <14>}>, 'cups.desktop': <{'position': <15>}>, 'nixos-manual.desktop': <{'position': <16>}>, 'org.gnome.seahorse.Application.desktop': <{'position': <17>}>, 'org.gnome.TextEditor.desktop': <{'position': <18>}>, 'org.gnome.Calendar.desktop': <{'position': <19>}>}, {'vim.desktop': <{'position': <0>}>, 'code.desktop': <{'position': <1>}>, 'vlc.desktop': <{'position': <2>}>, 'xterm.desktop': <{'position': <3>}>}];
-    "favorite-apps" = ['org.gnome.Nautilus.desktop', 'spotify.desktop', 'steam.desktop', 'firefox.desktop', 'discord.desktop', 'code.desktop', 'org.prismlauncher.PrismLauncher.desktop'];
-    "welcome-dialog-last-shown-version" = '49.2';
+    "app-picker-layout" = "[{'org.gnome.Contacts.desktop': <{'position': <0>}>, 'org.gnome.Weather.desktop': <{'position': <1>}>, 'org.gnome.clocks.desktop': <{'position': <2>}>, 'org.gnome.Maps.desktop': <{'position': <3>}>, 'org.gnome.SimpleScan.desktop': <{'position': <4>}>, 'org.gnome.Settings.desktop': <{'position': <5>}>, 'org.gnome.Showtime.desktop': <{'position': <6>}>, 'org.gnome.Snapshot.desktop': <{'position': <7>}>, 'Utilities': <{'position': <8>}>, 'System': <{'position': <9>}>, 'org.gnome.Console.desktop': <{'position': <10>}>, 'org.gnome.Yelp.desktop': <{'position': <11>}>, 'org.gnome.Calculator.desktop': <{'position': <12>}>, 'org.gnome.Extensions.desktop': <{'position': <13>}>, 'htop.desktop': <{'position': <14>}>, 'cups.desktop': <{'position': <15>}>, 'nixos-manual.desktop': <{'position': <16>}>, 'org.gnome.seahorse.Application.desktop': <{'position': <17>}>, 'org.gnome.TextEditor.desktop': <{'position': <18>}>, 'org.gnome.Calendar.desktop': <{'position': <19>}>}, {'vim.desktop': <{'position': <0>}>, 'code.desktop': <{'position': <1>}>, 'vlc.desktop': <{'position': <2>}>, 'xterm.desktop': <{'position': <3>}>}]";
+    "favorite-apps" = "['org.gnome.Nautilus.desktop', 'spotify.desktop', 'steam.desktop', 'firefox.desktop', 'discord.desktop', 'code.desktop', 'org.prismlauncher.PrismLauncher.desktop']";
+    "welcome-dialog-last-shown-version" = "'49.2'";
 
-    "locations" = @as [];
+    "locations" = "@as []";
   };
 }
