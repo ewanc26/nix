86be6518e600d1fa9e24138d863b208c30c3fd16
Ewan Croft <git@ewancroft.uk>
2026-02-08 18:04:31 +0000

    fix: replace empty dconf group with valid GNOME notifications schema

--- settings/gnome/desktop/notifications/notifications.nix
+++ settings/gnome/desktop/notifications/notifications.nix
@@ -5,21 +5,20 @@ with lib.hm.gvariant;
 
 {
   dconf.settings = {
-    "" = {
+    "org/gnome/desktop/notifications" = {
       application-children = [ "firefox" "gnome-about-panel" "gnome-power-panel" ];
     };
 
-    "application/firefox" = {
+    "org/gnome/desktop/notifications/application/firefox" = {
       application-id = "firefox.desktop";
     };
 
-    "application/gnome-about-panel" = {
+    "org/gnome/desktop/notifications/application/gnome-about-panel" = {
       application-id = "gnome-about-panel.desktop";
     };
 
-    "application/gnome-power-panel" = {
+    "org/gnome/desktop/notifications/application/gnome-power-panel" = {
       application-id = "gnome-power-panel.desktop";
     };
-
   };
 }
