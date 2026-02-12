220c2ada3ff38dc13ae53f89398789e7c7195fe2
Ewan Croft <git@ewancroft.uk>
2026-02-12 19:45:13 +0000

    feat: add minimal server host configuration

---
+++ hosts/server/default.nix
@@ -0,0 +1,42 @@
+{ config, pkgs, ... }:
+
+{
+  imports = [
+    ./hardware-configuration.nix
+    ../../modules/common.nix
+    ../../modules/users.nix
+    ../../modules/server-packages.nix
+    ../../modules/server-services.nix
+    ../../modules/git-backup.nix
+  ];
+
+  # Networking
+  networking = {
+    hostName = "server";
+    
+    # Firewall configuration
+    firewall = {
+      enable = true;
+      allowedTCPPorts = [ 22 ]; # SSH
+      # Add more ports as needed: 80 443 for web, etc.
+    };
+  };
+
+  # Boot configuration - clean /tmp on boot
+  boot.tmp.cleanOnBoot = true;
+
+  # User SSH keys for remote access
+  users.users.ewan.openssh.authorizedKeys.keys = [
+    # Add your SSH public keys here
+    # "ssh-ed25519 AAAAC3... user@host"
+  ];
+
+  # Security settings
+  security.sudo = {
+    enable = true;
+    wheelNeedsPassword = true;
+  };
+
+  # System version
+  system.stateVersion = "25.11";
+}
