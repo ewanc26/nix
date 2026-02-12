0bf36f7b19daddc70ce2c328eb0806010742a5ba
Ewan Croft <git@ewancroft.uk>
2026-02-12 22:30:42 +0000

    refactor: remove signing due to issues

--- home/programs/git.nix
+++ home/programs/git.nix
@@ -1,9 +1,5 @@
-{ config, pkgs, lib, ... }:
+{ config, pkgs, lib, osConfig, ... }:
 
-let
-  sshSignKey = "${config.home.homeDirectory}/.ssh/id_ed25519.pub";
-  haveKey = builtins.pathExists sshSignKey;
-in
 {
   programs.git = {
     enable = true;
@@ -11,7 +7,7 @@ in
 
     settings = {
       user = {
-        name = "Ewan Croft";
+        name  = "Ewan Croft";
         email = "git@ewancroft.uk";
       };
 
@@ -25,18 +21,10 @@ in
       init.defaultBranch = "main";
 
       alias = {
-        la = "log --all --graph --pretty=format:'%C(auto)%h%d %s %C(bold black)(%ar by <%aN>)%Creset'";
+        la  = "log --all --graph --pretty=format:'%C(auto)%h%d %s %C(bold black)(%ar by <%aN>)%Creset'";
         law = "log --all --graph --pretty=format:'%C(auto)%h%d %w(100,0,8)%s %C(bold black)(%ar by <%aN>)%Creset'";
         lad = "log --all --graph --pretty=format:'%Cgreen%ad%Creset %C(auto)%h%d %s %C(bold black)<%aN>%Creset' --date=format-local:'%Y-%m-%d %H:%M (%a)'";
       };
-    };
-
-    # only add signing config if key exists
-    extraConfig = lib.mkIf haveKey {
-      user.signingkey = sshSignKey;
-      commit.gpgsign = true;
-      tag.gpgsign = true;
-      gpg.format = "ssh";
     };
   };
 }
