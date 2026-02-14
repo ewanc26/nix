5b127595a6060f3ea84d927fdcdd484fd0fcbdd8
Ewan Croft <git@ewancroft.uk>
2026-02-14 12:30:14 +0000

    fix: escape bash variables in zsh SSH agent code

--- home/programs/zsh.nix
+++ home/programs/zsh.nix
@@ -66,16 +66,16 @@ in
 
       function start_agent {
         echo "Starting new SSH agent..."
-        ssh-agent | sed 's/^echo/#echo/' > "${SSH_ENV}"
-        chmod 600 "${SSH_ENV}"
-        . "${SSH_ENV}" > /dev/null
+        ssh-agent | sed 's/^echo/#echo/' > "''${SSH_ENV}"
+        chmod 600 "''${SSH_ENV}"
+        . "''${SSH_ENV}" > /dev/null
         ssh-add ~/.ssh/id_ed25519 2>/dev/null
       }
 
       # Check if agent is running
-      if [ -f "${SSH_ENV}" ]; then
-        . "${SSH_ENV}" > /dev/null
-        ps -ef | grep ${SSH_AGENT_PID} | grep ssh-agent$ > /dev/null || {
+      if [ -f "''${SSH_ENV}" ]; then
+        . "''${SSH_ENV}" > /dev/null
+        ps -ef | grep ''${SSH_AGENT_PID} | grep ssh-agent$ > /dev/null || {
           start_agent;
         }
       else
